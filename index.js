619af30c445a0ca1cec346ebd37b499291da86d3
Syed Wajahat Ali <wajahat.klk25.com>
2022-05-27 17:20:12 +0200

    added avg of students with loan

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const fakeApi = require('./data');
 const app = express();
 
 /* Your code goes here */
+app.get('/', (req, res)=> {
+    res.send('<h1>hello world!</h1>')
+})
+
+app.get('/loan', (req,res) => {
+    let hasLoanT = [];
+    fakeApi.forEach(student => {
+        if (student.hasLoan){
+            hasLoanT.push(student)
+        }
+    })
+    res.send(hasLoanT);
+    
+});
+
+app.get('/source', (req, res) => {
+    let num = (2/fakeApi.length)
+    num.toFixed(2);
+    res.send(`<h2>The group's average score is ${num}</h2>`);
+    })
+    
+    
+
+
 
 // Server listening
 app.listen(3000, () => console.log('🚀 My first app listening on port 3000! '));
